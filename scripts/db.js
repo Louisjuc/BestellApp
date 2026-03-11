25f95d93855615a6a68103ff8a2058980ed14b33
louisj <louis.juchem@web.de>
2026-03-11 18:57:47 +0100

    variable pictues and basket added

--- scripts/db.js
+++ scripts/db.js
@@ -1,17 +1,21 @@
 let foods = [
     {
+      "img" : "veggie_burger.png",
       "name": "Veggie mushroom black burger",
       "description": "Mixed green salad, Tomatoes, Edamame, Mushrooms",
      "price" : "13,90€"
     },    {
+      "img" : "all_meat.png",
         "name": "All meat burger",
         "description": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
        "price" : "15,90€"
       },{
+        "img" : "beef_red.png",
         "name": "Beef red burger",
         "description": "Beef, Cheese, Tomatoes, Lettuce, Onion",
        "price" : "14,90€"
       },{
+        "img" : "big_chicken.png",
         "name": "Big chicken burger",
         "description": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
        "price" : "15,90€"
--- scripts/template.js
+++ scripts/template.js
@@ -2,7 +2,7 @@ function foodTemplate(index){
     return `        <section id="food_template_section">
     <div class="food_card">
     <div class="food_info">
-        <img src="./assets/img/Rectangle 10-1.png" class="food_img">
+        <img src="./assets/img/${foods[index].img}" class="food_img">
         <div>
         <h3>${foods[index].name}</h3>
         <p>${foods[index].description}</p>
