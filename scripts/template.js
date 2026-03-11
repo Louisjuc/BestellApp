f6e0cbe1023fce205ca7548a7eb65d739c4fcbfd
louisj <louis.juchem@web.de>
2026-03-11 16:50:45 +0100

    space between infos fixed

--- scripts/template.js
+++ scripts/template.js
@@ -1,10 +1,12 @@
 function foodTemplate(index){
     return `        <section id="food_template_section">
     <div class="food_card">
+    <div class="food_info">
         <img src="./assets/img/Rectangle 10-1.png" class="food_img">
         <div>
         <h3>${foods[index].name}</h3>
         <p>${foods[index].description}</p>
+        </div>
     </div>
         <div class="buy_area">
         <p>${foods[index].price}</p>
