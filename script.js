93e61d65460a77b36a7b0a78a0ec8f84ba18941d
louisj <louis.juchem@web.de>
2026-03-12 16:08:41 +0100

    math added

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-function render(){
-    let contentRef = document.getElementById("menu");
-    contentRef.innerHTML += "";
-    for (let index = 0; index < foods.length; index++) {
-      contentRef.innerHTML += foodTemplate(index);
-    }
+function render() {
+  let contentRef = document.getElementById("menu");
+  contentRef.innerHTML += "";
+  for (let index = 0; index < foods.length; index++) {
+    contentRef.innerHTML += foodTemplate(index);
+  }
 }
 
-function addBasket(){
-   let contentRef = document.getElementById('Basekt')
-    
+function addtoBasket(index, button) {
+  let itemRef = document.getElementById("item-basket");
+  itemRef.innerHTML += `${itemTemplate(index)}`;
+  button.innerText = "Added";
+  if (button.innerText === "Added") {
+    button.disabled = true;
+  }
+}
+
+function addItem(index){
+  let counter = document.querySelectorAll('.food_counter');
+  counter[index].innerHTML = numberIndex++;
 }
--- scripts/template.js
+++ scripts/template.js
@@ -1,3 +1,5 @@
+let numberIndex = 1;
+
 function foodTemplate(index){
     return `        <section id="food_template_section">
     <div class="food_card">
@@ -9,10 +11,23 @@ function foodTemplate(index){
         </div>
     </div>
         <div class="buy_area">
-        <p>${foods[index].price}</p>
-        <button class="buy" onclick="addBasket">Add to basket</button>
+        <p>${foods[index].price.toFixed(2) + "€"}</p>
+        <button class="buy" onclick="addtoBasket(${index}, this)">Add to basket</button>
     </div>
     </div>
 </section>
+    `
+}
+
+function itemTemplate(index){
+    return ` <div class="item">
+    <h4>${foods[index].name}</h4>
+    <section class="basket_numbers">
+    <img src="./assets/icons/delete.svg" class="delete">
+    <p class="food_counter">${numberIndex}</p>
+    <button class="add_item" onclick="addItem(${index})">+</button>
+    <p>${foods[index].price.toFixed(2) + "€"}</p>
+    </section>
+   </div> 
     `
 }
